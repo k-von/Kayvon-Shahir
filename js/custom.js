6c066d8ae8bbedb5d30bdbeb6fdee4ef8474808d
Kayvon Shahir <kayvons@icloud.com>
2018-04-22 13:29:41 -0700

    merging from remote

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,5 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function(event) {
 
-    var hamburger = document.getElementById("hamburger");
-    hamburger.addEventListener("click", function (event) {
-
-        var myMenu = document.getElementById("menu");
-
-        if (myMenu.className == "close-menu") {
-            hamburger.className = "ham-open";
-            myMenu.className = "open-menu";
-        } else {
-            hamburger.className = "";
-            myMenu.className = "close-menu";
-        }
-    }, false);
-
+  
 
 });
