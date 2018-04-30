35421834166235590cecc21aa6bfea2432082287
Kayvon Shahir <kayvons@icloud.com>
2018-04-30 11:49:05 -0700

    made it mobile  responsive friendly and created webpack package for it

---
+++ js/index.js
@@ -0,0 +1,24 @@
+window.$ = require('jquery');
+require('popper.js');
+import 'bootstrap';
+require('./custom.js');
+require('../scss/main.scss');
+
+function getComponent() {
+  return import ( /* webpackChunkName: 'lodash' */ 'lodash')
+    .then(_ => {
+      var element = document.createElement('div');
+
+      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+      return element;
+    })
+    .catch(error => {
+      'An error occured while loading component'
+    });
+}
+
+getComponent()
+  .then(component => {
+    document.body.appendChild(component);
+  });
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './js/index.js',
+  plugins: [new HTMLWebpackPlugin({
+    title: 'Title',
+    template: './index.template.html',
+    inject: 'head',
+    filename: '../index.html'
+  })],
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.(scss)$/,
+      use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            plugins: function() {
+              return [require('precss'), require('autoprefixer')];
+            }
+          }
+        },
+        {
+          loader: 'sass-loader'
+        }
+      ]
+    }, {
+      test: /\.(css)$/,
+      use: [{
+        loader: 'css-loader'
+      }]
+    }]
+
+  }
+
+}
